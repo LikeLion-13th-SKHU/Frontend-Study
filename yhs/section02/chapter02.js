1a9c30da9db3c67fd015943f54fcdd6c9fbeeef8
Yun, HyunSeung <hyun136000@gamil.com>
2025-01-13 22:46:13 +0900

    section2 시작

---
+++ yhs/section02/chapter02.js
@@ -0,0 +1,18 @@
+function returnFalse() {
+  return false;
+}
+
+function returnTrue() {
+  return true;
+}
+
+console.log(returnFalse() && returnTrue());
+
+// 단락 평가 활용 사례
+
+function printName(person) {
+  const name = person && person.name;
+  console.log(name && "person의 값이 없음");
+}
+
+printName();
