ba39e8673f32fbf5a4606cc394bd4ba925cb2056
Yun, HyunSeung <hyun136000@gamil.com>
2025-02-03 21:05:36 +0900

    나머지 커밋!

--- yhs/section05/src/App.jsx
+++ yhs/section05/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { useState } from "react";
 import Register from "./components/Register";
+import HookExam from "./components/HookExam";
 
 // 컴포넌트로 기능을 나누어 저장하면 리렌더링 시 불필요한 반복적인 App 선언을 막을 수 있다.
 function App() {
   return (
     <>
-      <Register />
+      <HookExam />
     </>
   );
 }
